8c75aab5e9ccdd0dad16b5cd69fcf2ccfa2aa463
Scuobahia <antonio.sr8808@gmail.comgit config --global user.name Antonio>
2021-05-20 12:19:28 -0600

    having some issues with Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,17 @@
 var startBtn = document.getElementById('start-btn')
 var questionBoxEl = document.getElementById('question-box')
+var questionEl = getElementById("question")
+var answerBtn = getElementById('answer-buttons')
+var randomQuestions, currentQuestionIndex
 // Array of Questions and Answers
 var questions = [
 {
-    Question: 'Which of the following is not JavaScript Data Types?'
+    question: 'Which of the following is not JavaScript Data Types?',
     answers:[
-        {text:'Undefined', correct:false}
-        {text:'Number', correct:false}
-        {text:'Boolean', correct:false}
-        {text:'Float', correct:true}
-
+        { text:'Undefined', correct:false},
+        { text:'Number', correct:false },
+        { text:'Boolean', correct:false },
+        { text:'Float', correct:true }
     ]
 }
     
@@ -17,11 +19,17 @@ var questions = [
 startBtn.addEventListener("click", startGame)
 function startGame() {
 startBtn.classList.add("hide")
+randomQuestions = questions.sort(() => Math.random() - .5)
+currentQuestionIndex = 0
 questionBoxEl.classList.remove("hide")
 nextQuestion
 }
 function nextQuestion() {
+    whatQuestion(randomQuestions[currentQuestionIndex])
 
+}
+function whatQuestion(question) {
+questionEl.innerText = question.question
 }
 function nextAnswer() {
 
